f2e835167abb91f6af8e451d54efecfc58332929
Christoffer Slettan <christofferslettan@gmail.com>
2021-11-10 11:00:33 +0100

    Add appsettings.json to REST API sample app
    + remove some unnecessary dependencies

--- RestAPI/Server/Server.cs
+++ RestAPI/Server/Server.cs
@@ -7,76 +7,84 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using System.Threading.Tasks;
-using Idfy;
-using Idfy.IdentificationV2;
+using Signicat.Express;
+using Signicat.Express.IdentificationV2;
 
-namespace Server 
+namespace Server
 {
-    class Server 
+    class Server
     {
         public static void Main(string[] args)
         {
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://0.0.0.0:4242")
+                .UseUrls("http://localhost:4242")
                 .UseWebRoot(".")
                 .UseStartup<Startup>()
                 .Build()
                 .Run();
         }
-    }   
+    }
 
     public class Startup
-    {        
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().AddNewtonsoftJson(); 
+            services.AddMvc().AddNewtonsoftJson();
+
+            services.AddSingleton<IIdentificationV2Service>(c => new IdentificationV2Service(
+                Configuration["Signicat:ClientId"],
+                Configuration["Signicat:ClientSecret"],
+                new List<OAuthScope>() { OAuthScope.Identify }
+            ));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment()) app.UseDeveloperExceptionPage(); 
-            app.UseRouting();
-            app.UseStaticFiles(); 
-            app.UseEndpoints(endpoints => endpoints.MapControllers()); 
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 
 
-    [Route("authentication-session")] 
-    [ApiController] 
+    [Route("authentication-session")]
+    [ApiController]
     public class AuthenticationApiController : Controller
     {
-        private readonly IIdentificationV2Service _identificationV2Service;
+        private readonly IIdentificationV2Service _identificationService;
+        private readonly string _frontendAppUrl;
+        private readonly string _backendUrl;
 
-        public AuthenticationApiController()
+        public AuthenticationApiController(IIdentificationV2Service identificationService, IConfiguration configuration)
         {
-            // Todo: Fill in your API Credential
-            _identificationV2Service = new IdentificationV2Service(
-                "<INSERT CLIENT_ID>",
-                "<INSERT CLIENT_SECRET>",
-                new List<OAuthScope>
-                {
-                    OAuthScope.Identify
-                });
+            _identificationService = identificationService;
+            _frontendAppUrl = configuration["FrontendAppUrl"];
+            _backendUrl = configuration["BackendUrl"];
         }
 
-        [HttpPost] 
-        public async Task<ActionResult> Create() 
+        [HttpPost]
+        public async Task<ActionResult> Create()
         {
-            var domain = "http://localhost:3000/";
-            var backendDomain = "http://localhost:4242/";
-
-            var session = await _identificationV2Service.CreateSessionAsync(new IdSessionCreateOptions()
+            var session = await _identificationService.CreateSessionAsync(new IdSessionCreateOptions()
             {
                 Flow = IdSessionFlow.Redirect,
                 RedirectSettings = new RedirectSettings()
                 {
-                    ErrorUrl = domain + "?error=true",
-                    AbortUrl = domain + "?canceled=true",
-                    SuccessUrl = backendDomain + "authentication-session"
+                    ErrorUrl = _frontendAppUrl + "?error=true",
+                    AbortUrl = _frontendAppUrl + "?canceled=true",
+                    SuccessUrl = _backendUrl + "authentication-session"
                 },
                 AllowedProviders = new List<IdProviderType>
                 {
@@ -87,25 +95,24 @@ namespace Server
                 },
                 Include = new List<Include>()
                 {
-                   Include.Name,
-                   Include.Nin
+                    Include.Name,
+                    Include.Nin
                 }
             });
 
-            Response.Headers.Add("Location", session.Url); 
-            return new StatusCodeResult(303); 
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
         }
-        
+
         [HttpGet]
         public async Task<ActionResult> Retrieve([FromQuery(Name = "sessionId")] string sessionId)
         {
-            var domain = "http://localhost:3000/";
+            var result = await _identificationService.GetSessionAsync(sessionId);
 
-            var result = await _identificationV2Service.GetSessionAsync(sessionId);
-            string name = result.Identity.FullName;
-            string nin = result.Identity.Nin;
+            var name = result.Identity.FullName;
+            var nin = result.Identity.Nin;
 
-            Response.Headers.Add("Location", domain + "?success=true&name=" + name + "&nin=" + nin);
+            Response.Headers.Add("Location", _frontendAppUrl + "?success=true&name=" + name + "&nin=" + nin);
             return new StatusCodeResult(303);
         }
     }
