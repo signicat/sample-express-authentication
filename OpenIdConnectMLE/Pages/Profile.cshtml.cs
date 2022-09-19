154c8e84ecdf90086fc4a173ddbc99166e3cd8c7
Bendik Simonsen <Bensim@signicat.com>
2022-09-19 12:15:56 +0200

    Sample OIDC FTN app

---
+++ OpenIdConnectMLE/Pages/Profile.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SampleApp.Pages
+{
+    [Authorize]
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
